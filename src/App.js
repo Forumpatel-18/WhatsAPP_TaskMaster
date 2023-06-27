0cab310bfd30066cb5e733b8354d6dfff5a54ef5
Forum Patel <forum180301@gmail.com>
2023-06-27 11:55:22 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from "react";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import Close from "./components/Close";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Tasksummary from "./components/Tasksummary";
 
 const App = () => {
   return (
     <div className="wrapper">
-      <BrowserRouter>
+      <BrowserRouter basename="/taskmaster">
         <Routes>
           <Route path="/" element={<Login />}></Route>
           <Route path="/tasksummary" element={<Tasksummary />}></Route>
